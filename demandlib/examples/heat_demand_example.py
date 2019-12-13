90570d80f6f80659127542ffb14af5215f509d3c
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2019-12-13 13:32:00 +0100

    Blacked

--- demandlib/examples/heat_demand_example.py
+++ demandlib/examples/heat_demand_example.py
@@ -16,6 +16,7 @@ import pandas as pd
 import demandlib.bdew as bdew
 import datetime
 import os
+
 try:
     import matplotlib.pyplot as plt
 except ImportError:
@@ -23,7 +24,7 @@ except ImportError:
 
 # read example temperature series
 
-datapath = os.path.join(os.path.dirname(__file__), 'example_data.csv')
+datapath = os.path.join(os.path.dirname(__file__), "example_data.csv")
 temperature = pd.read_csv(datapath)["temperature"]
 
 
@@ -36,40 +37,56 @@ temperature = pd.read_csv(datapath)["temperature"]
 
 def heat_example():
     holidays = {
-        datetime.date(2010, 5, 24): 'Whit Monday',
-        datetime.date(2010, 4, 5): 'Easter Monday',
-        datetime.date(2010, 5, 13): 'Ascension Thursday',
-        datetime.date(2010, 1, 1): 'New year',
-        datetime.date(2010, 10, 3): 'Day of German Unity',
-        datetime.date(2010, 12, 25): 'Christmas Day',
-        datetime.date(2010, 5, 1): 'Labour Day',
-        datetime.date(2010, 4, 2): 'Good Friday',
-        datetime.date(2010, 12, 26): 'Second Christmas Day'}
+        datetime.date(2010, 5, 24): "Whit Monday",
+        datetime.date(2010, 4, 5): "Easter Monday",
+        datetime.date(2010, 5, 13): "Ascension Thursday",
+        datetime.date(2010, 1, 1): "New year",
+        datetime.date(2010, 10, 3): "Day of German Unity",
+        datetime.date(2010, 12, 25): "Christmas Day",
+        datetime.date(2010, 5, 1): "Labour Day",
+        datetime.date(2010, 4, 2): "Good Friday",
+        datetime.date(2010, 12, 26): "Second Christmas Day",
+    }
 
     # Create DataFrame for 2010
     demand = pd.DataFrame(
-        index=pd.date_range(pd.datetime(2010, 1, 1, 0),
-                            periods=8760, freq='H'))
-    
+        index=pd.date_range(pd.datetime(2010, 1, 1, 0), periods=8760, freq="H")
+    )
+
     # Single family house (efh: Einfamilienhaus)
-    demand['efh'] = bdew.HeatBuilding(
-        demand.index, holidays=holidays, temperature=temperature,
-        shlp_type='EFH',
-        building_class=1, wind_class=1, annual_heat_demand=25000,
-        name='EFH').get_bdew_profile()
+    demand["efh"] = bdew.HeatBuilding(
+        demand.index,
+        holidays=holidays,
+        temperature=temperature,
+        shlp_type="EFH",
+        building_class=1,
+        wind_class=1,
+        annual_heat_demand=25000,
+        name="EFH",
+    ).get_bdew_profile()
 
     # Multi family house (mfh: Mehrfamilienhaus)
-    demand['mfh'] = bdew.HeatBuilding(
-        demand.index, holidays=holidays, temperature=temperature,
-        shlp_type='MFH',
-        building_class=2, wind_class=0, annual_heat_demand=80000,
-        name='MFH').get_bdew_profile()
+    demand["mfh"] = bdew.HeatBuilding(
+        demand.index,
+        holidays=holidays,
+        temperature=temperature,
+        shlp_type="MFH",
+        building_class=2,
+        wind_class=0,
+        annual_heat_demand=80000,
+        name="MFH",
+    ).get_bdew_profile()
 
     # Industry, trade, service (ghd: Gewerbe, Handel, Dienstleistung)
-    demand['ghd'] = bdew.HeatBuilding(
-        demand.index, holidays=holidays, temperature=temperature,
-        shlp_type='ghd', wind_class=0, annual_heat_demand=140000,
-        name='ghd').get_bdew_profile()
+    demand["ghd"] = bdew.HeatBuilding(
+        demand.index,
+        holidays=holidays,
+        temperature=temperature,
+        shlp_type="ghd",
+        wind_class=0,
+        annual_heat_demand=140000,
+        name="ghd",
+    ).get_bdew_profile()
 
     if plt is not None:
         # Plot demand of building
@@ -78,8 +95,8 @@ def heat_example():
         ax.set_ylabel("Heat demand in kW")
         plt.show()
     else:
-        print('Annual consumption: \n{}'.format(demand.sum()))
+        print("Annual consumption: \n{}".format(demand.sum()))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     heat_example()
--- setup.py
+++ setup.py
@@ -11,18 +11,19 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(name='demandlib',
-      version='0.1.7dev',
-      author='oemof developer group',
-      url='https://oemof.org/',
-      license='GPL3',
-      author_email='oemof@rl-institut.de',
-      description='Demandlib of the open energy modelling framework',
-      long_description=read('README.rst'),
-      packages=find_packages(),
-      install_requires=['numpy >= 1.7.0, < 1.17',
-                        'pandas >= 0.18.0, < 0.25'],
-      package_data={
-          'demandlib': [os.path.join('bdew_data', '*.csv')],
-          'demandlib.examples': ['*.csv']},
-      )
+setup(
+    name="demandlib",
+    version="0.1.7dev",
+    author="oemof developer group",
+    url="https://oemof.org/",
+    license="GPL3",
+    author_email="oemof@rl-institut.de",
+    description="Demandlib of the open energy modelling framework",
+    long_description=read("README.rst"),
+    packages=find_packages(),
+    install_requires=["numpy >= 1.7.0, < 1.17", "pandas >= 0.18.0, < 0.25"],
+    package_data={
+        "demandlib": [os.path.join("bdew_data", "*.csv")],
+        "demandlib.examples": ["*.csv"],
+    },
+)
